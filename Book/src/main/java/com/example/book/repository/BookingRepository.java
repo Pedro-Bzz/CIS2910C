847ed5672504b611a66bb1c0c9b6e3fd450b1847
Pedro-Bzz <pedrosilva.address@gmail.com>
2025-07-26 14:22:33 -0400

    Delete Book directory

--- Book/src/main/java/com/example/book/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.book.repository;
-
-import com.example.book.model.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-    List<Booking> findByStudentEmail(String email);
-}
--- Book/src/main/java/com/example/book/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.book.repository;
-
-import com.example.book.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, String> {
-    // String because Student's ID is email (String)
-}
